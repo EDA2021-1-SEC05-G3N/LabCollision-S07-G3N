76baa00403f86a6df97880b5b803a9fe54df44cf
eduardoh27 <ej.herreraa@uniandes.edu.co>
2021-04-05 19:58:56 -0500

    laboratorio 7 - Entrega final 7

--- App/view.py
+++ App/view.py
@@ -175,7 +175,7 @@ while True:
     elif int(inputs[0]) == 6:
         # TODO: modificaciones para observar el tiempo y memoria
         number = input("Buscando libros del año?: ")
-            
+        fraction = input("Fraccion de libros en el año? (entre 0.0 y 1.0): ")
         rank = input("Cuantos libros en el escalafon? (mayor a 0): ")
         number = int(number)
         fraction = float(fraction)
